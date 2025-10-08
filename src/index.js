953d0cd2a43d52c9e52aa3d860fe237b7c859893
shimoine <p7bz0x0n@s.okayama-u.ac.jp>
2025-10-08 10:59:55 +0900

    Modify to resolve Warning Messages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route, Link, Switch, withRouter} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
